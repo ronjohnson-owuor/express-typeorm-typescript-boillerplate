e4e0fa8816350e69c45414ff5de5e766e09e81b1
Ronjohnson Owuor <ronjohnsonowuor83@gmail.com>
2025-03-07 22:14:03 +0300

    added build feature to the boiler plate

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,18 +1,30 @@
-import "reflect-metadata"
-import { DataSource } from "typeorm"
 import * as dotenv from "dotenv";
+import "reflect-metadata";
+import { DataSource } from "typeorm";
 dotenv.config();
 
-let dbType:any = (process.env.DB_TYPE) || 'mysql';
+let dbType: any = process.env.DB_TYPE || "mysql";
+let environment = process.env.APP_ENV;
+if (!environment) {
+	console.log(
+		"set APP_ENV= variable  in your env file or else there will be an error in accessing databases"
+	);
+}
 export const AppDataSource = new DataSource({
-    type:dbType,
-    host:process.env.DB_HOST,
-    port: Number(process.env.DB_PORT!) || 3306,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    entities:['src/entity/*.ts'],
-    migrations:['src/migrations/*.ts'],
-    timezone:"Z",
-    synchronize:false
-})
+	type: dbType,
+	host: process.env.DB_HOST,
+	port: Number(process.env.DB_PORT!) || 3306,
+	username: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_NAME,
+	entities: [
+		environment == "development" ? "src/entity/*.ts" : "dist/src/entity/*.js",
+	],
+	migrations: [
+		environment == "development"
+			? "src/migrations/*.ts"
+			: "dist/src/migrations/*.js",
+	],
+	synchronize: false,
+	timezone: "Z",
+});
